434734873b91ed76359ed477d8a615750ae6f3b0
DanielRailean <daniel.railean1@gmail.com>
2025-08-15 17:53:29 +0200

    push

--- test.js
+++ test.js
@@ -5,18 +5,31 @@ function main(){
   const returnPerYear = 10
   const depositPerMonth = 1000
   const startingBalance = 0
+  const yearlyTax = 15.3
+  const earlyPenalty = 60
 
   let balance = startingBalance
 
+  let totalTax = 0
   for (let current = 1; current <= years; current++) {
+    let yearInterest = 0
     for (let return_period = 0; return_period < timesPerYear; return_period++) {
+      const deposit = depositPerMonth * (12 / timesPerYear)
       const interest = (balance * (returnPerYear / timesPerYear)) / 100
       // console.log(interest)
+      yearInterest = yearInterest + interest
       balance = deposit + interest + balance
     }
-    const deposit = depositPerMonth * (12 / timesPerYear)
-    console.log(balance)
+
+    console.log("interest: "+yearInterest)
+    const tax = (yearInterest) * yearlyTax / 100
+    totalTax = totalTax + tax
+    balance = balance - tax
+    console.log(tax)
+    console.log("after tax: " + balance)
   }
+  console.log(totalTax)
+  console.log(`final: ${balance * (100 - earlyPenalty) / 100}`)
 }
 
 main()
---
+++ v2.js
@@ -0,0 +1,28 @@
+
+function main(){
+  const years = 20
+  const timesPerYear = 12
+  const returnPerYear = 10
+  const depositPerMonth = 1000
+  const startingBalance = 0
+  const yearlyTax
+
+  let balance = startingBalance
+
+  for (let year = 1; year <= years; year++) {
+    for (let half = 0; half <= 2; half++) {
+      for (let quarter = 0; quarter <= 2; quarter++) {
+        for (let month = 0; month <= 3; month++) {
+          for (let two_week = 0; two_week <= 2; two_week++) {
+            for (let week = 0; week <= 2; week++) {
+                for (let day = 0; day <= 7; half++) {
+                }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+main()
